28f9118733acbe88b93eab79c417a7803a9b4254
pkritika <kritika.pant@bison.howard.edu>
2025-02-15 18:00:37 -0500

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Kritika  on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
